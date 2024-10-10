73440f362cf06097a17634c5a96b4e0d48844d4c
Umar Farouq <umarfarouq447@gmail.com>
2024-10-10 13:43:37 +0100

    a

---
+++ x.js
@@ -0,0 +1,14 @@
+async function start() {
+    try {
+        const data = await fetch('https://worldtimeapi.org/api/timezone/America/New_York');
+        if (!data.ok) {
+            throw new Error('Bad network');
+        }
+        const result = await data.json();
+        console.log(result.time); // Log result.time instead of data.time
+    } catch (error) {
+        console.log('ERROR: ', error);
+    }
+}
+
+start();
